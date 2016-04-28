ff0bd643eba420ffd5f02f62d47b7d1872882dbe
汪强 <jonwang@xindong.com>
2016-04-28 18:43:31 +0800

    优化样式

--- admin/task/list.php
+++ admin/task/list.php
@@ -34,7 +34,7 @@ unset($item);
                     foreach ($query['sql'] as $sql)
                     {
                         $i++;
-                        echo "<tr><td>{$i}</td><td><pre>$sql</pre></td><td align=\"center\"><button type=\"button\" class=\"btn btn-danger btn-sm\">删除</button></td></tr>";
+                        echo "<tr><td>{$i}</td><td>$sql</td><td align=\"center\"><button type=\"button\" class=\"btn btn-danger btn-sm\">删除</button></td></tr>";
                     }
                 }
                 ?>
