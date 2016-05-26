cd0025b2f27b96c0c14b5ac5f84f6853fede797d
龚少瑜 <gongshaoyu@xindong.com>
2016-05-26 18:39:51 +0800

    统计序列管理6

--- admin/series/detail.php
+++ admin/series/detail.php
@@ -1,88 +1,62 @@
+<?php
+    try
+    {
+        $seriesKey = $this->request->get['key'];
+
+        if (!$seriesKey)
+        {
+            throw new Exception("缺失序列参数!");
+        }
+
+        if ($this->worker->isSSDB)
+        {
+            $serieDetail = unserialize($this->worker->ssdb->hget('series', $seriesKey));
+        }
+        else
+        {
+            $serieDetail = unserialize($this->worker->redis->hget('series', $seriesKey));
+        }
+
+        if (!$serieDetail)
+        {
+            throw new Exception("无次序列信息详情!");
+        }
+
+        $data = array('status' => true, 'serie_detail' => $serieDetail);
+    }
+    catch (Exception $e)
+    {
+        $data = array('status' => false, 'message' => $e->getMessage());
+    }
+?>
+
 <div style="padding:0 15px;margin-top:-15px">
     <div class="row">
         <div class="col-md-12">
             <div class="pull-left" style="padding:10px 0 0 0">
                 <a href="/admin/">管理首页</a> <i style="font-size:11px;width:1em" class="glyphicon glyphicon-menu-right"></i>
-                统计序列管理
+                <a href="/admin/series/list/">任务列表</a> <i style="font-size:11px;width:1em" class="glyphicon glyphicon-menu-right"></i>
+                统计序列详情
             </div>
         </div>
     </div>
-
-    <table class="table table-bordered table-striped table-hover">
-        <thead>
-            <tr style="white-space:nowrap">
-                <th style="text-align:center" width="50">统计类型</th>
-                <th width="120" style="text-align:center">时间单位</th>
-                <th width="45" style="text-align:center">所属游戏</th>
-                <th width="130" style="text-align:center">时间</th>
-                <th width="140" style="text-align:center">结果</th>
-            </tr>
-        </thead>
-        <tbody id="ssdb_data_boday"></tbody>
-    </table>
-    <ul class="pager"></ul>
+<?php
+    if (!$data['status'])
+    {
+?>
+        <p class="bg-danger"><?php echo $data['message'];?></p>
+<?php
+    }else{
+?>
+        <div class="col-md-12">
+            <h4>序列配置详情</h4>
+            <?php
+                echo '<pre style="background: #fcfcfc"><code class="json">';
+                echo htmlspecialchars(json_encode($serieDetail, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE));
+                echo '</code></pre>';
+            ?>
+        </div>
+<?php
+    }
+?>
 </div>
-
-<script type="text/javascript">
-    get_list('');
-
-    function get_list(formData)
-    {
-        $.ajax({
-            url: '/api/task/series',
-            data: formData,
-            type: 'get',
-            dataType: 'json',
-            success: function(data, status, xhr)
-            {
-                if (data.status == 'error')
-                {
-                    alert(data.message || '查询失败');
-                    return false;
-                }
-
-                var html = '';
-                $.each(data.list, function(name, value) {
-                    html += '<tr>';
-                    html += '<td style="text-align:center" >'+value.tab+'</td>';
-                    html += '<td style="text-align:center" >'+value.time_unit+'</td>';
-                    html += '<td style="text-align:center" >'+value.game+'</td>';
-                    html += '<td style="text-align:center" >'+value.time+'</td>';
-                    html += '<td style="text-align:center" >'+value.result+'</td>';
-                    html += '</tr>';
-                });
-                $('#ssdb_data_boday').append(html);
-
-                $('.pager').html('');
-                var pager_html = '';
-                if (data.is_ssdb == false)
-                {
-                    if (data.next_iterator == 0)
-                    {
-                        pager_html += '<li class="disabled"><a href="javascript:void(0);">更多</a></li>';
-                    }else{
-                        pager_html += '<li><a href="javascript:void(0);" onclick="next_page(\'' + data.next_iterator + '\');">更多</a></li>';
-                    }
-                }else{
-
-                }
-
-                $('.pager').html(pager_html);
-            },
-            error: function(xhr, status, err)
-            {
-                alert('请求服务器失败');
-                return false;
-            }
-        });
-    }
-
-    function next_page(next_iterator)
-    {
-        var formData = {
-            page_type     : 'next',
-            next_iterator : next_iterator
-        };
-        get_list(formData);
-    }
-</script>
